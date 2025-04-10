f7d4f8be4be13b876c7bba677624f60880f2d1fd
Abrham Kindie <abrhamkindie805@gmail.com>
2025-04-10 15:17:01 +0300

    updated contents on the app.js i changed the almost the entire logic for the todo and doncounte code,i add a toggldon function for doncount

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,30 @@ import List from './components/List';
 export default function App(){
   const [item, setItem] = useState("");
   const [list, setList] = useState([]);
-
+ 
   const handleChange = (e) =>{
     const {value} = e.target; 
     setItem(value);
   }
 
-  function handleClick(){
+  function handleClick() {
     if (item.trim() === "") return;
-    setList(prevValue => [...prevValue, item]);
+    setList(prevValue => [...prevValue, { text: item, done: false }]);
     setItem("");
   }
-
 
   
   function deleteItem(i){
     const nValues = list.filter((e, index) => i !== index);
     setList(nValues);
   }
-
+  const toggleDone = (i) => {
+    setList(prev =>
+      prev.map((task, index) =>
+        index === i ? { ...task, done: !task.done } : task
+      )
+    );
+  };
  
   const doneCount = list.filter(item => item.done).length;
   const todoCount = list.length - doneCount;
@@ -41,10 +46,11 @@ export default function App(){
         controlId="floatingInput"
         label="Todo"
       >
-        <Form.Control  className='todoInput' type="text" placeholder='add todo' onChange={handleChange} value={item}/>
+        <Form.Control   className='todoInput' type="text" placeholder='add todo' onChange={handleChange}  value={item}/>
       </FloatingLabel>
       <Button className='addItem btn-outline-light' onClick={handleClick}>Add Item</Button>
-            <List list={list} deleteItem={deleteItem} />
+            <List list={list} deleteItem={deleteItem} toggleDone={toggleDone} />
+ 
 
        <div className='counter d-flex justify-content-around py-2'>
         <span>Total Items: {list.length}</span>
@@ -55,3 +61,4 @@ export default function App(){
   )
 }
 
+  
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,13 +1,14 @@
 import { v4 as itemKey } from 'uuid';
 import Item from './Item';
 
-export default function List({list, deleteItem}){
+export default function List({list, deleteItem,toggleDone}){
     
     function list_Items(){
         return list.map((e, i) => 
         <Item
          key={itemKey()}
          remove={()=> {deleteItem(i)}} 
+         toggleDone={() => toggleDone(i)}
           todo={e}/>)
     }
 
